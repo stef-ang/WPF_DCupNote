c3dc712f96d98a43e954acecb43fd155609b6823
stef-ang <anggara.stefanus@gmail.com>
2014-09-08 16:18:09 +0700

    road to drawing tools
    road to drawing tools

--- MainWindow.DrawingTools.cs
+++ MainWindow.DrawingTools.cs
@@ -3,23 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace WPF_DCupNote
 {
     partial class MainWindow
     {
         private string tool = null;
-
+        Point start, end;
+        Line drawLine;
+        
         private void MainCanvasMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // cek
+            switch (tool)
+            {
+                case "Line":
+                    start = e.GetPosition(mainCanvas);
+                    return;
+                case "Circle":
+                    return;
+                case "Rectangle":
+                    return;
+                default:
+                    return;
+            }
         }
 
         private void MainCanvasMouseMove(object sender, MouseEventArgs e)
         {
-            // cek dfnvj
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                switch (tool)
+                {
+                    case "Line":
+                        // pop line trakhir
+                        // push line yg baru
+                        end = e.GetPosition(mainCanvas);
+                        drawLine = new Line();
+                        drawLine.X1 = start.X;
+                        drawLine.Y1 = start.Y;
+                        drawLine.X2 = end.X;
+                        drawLine.Y2 = end.Y;
+                        drawLine.StrokeThickness = Convert.ToDouble(((ComboBoxItem)thicknessValue.SelectedItem).Content);
+                        drawLine.Stroke = new SolidColorBrush(colPicker.SelectedColor);
+                        mainCanvas.Children.Add(drawLine);
+                        Canvas.SetZIndex(drawLine, 1);
+                        return;
+                    case "Circle":
+                        return;
+                    case "Rectangle":
+                        return;
+                    default:
+                        return;
+                }
 
+                // clear
+                // gambar 4 stack
+            }
         }
 
         private void MainCanvasMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -29,7 +73,6 @@ namespace WPF_DCupNote
 
         private void NormalizationToolsDrawing()
         {
-            this.tool = null;
             lineBtn.Opacity = circleBtn.Opacity = rectangleBtn.Opacity = 1;
         }
 
@@ -42,6 +85,8 @@ namespace WPF_DCupNote
                 tool = "Line";
                 lineBtn.Opacity = 0.6;
             }
+            else
+                tool = null;
         }
 
         private void RectangleBtnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -53,6 +98,8 @@ namespace WPF_DCupNote
                 tool = "Rectangle";
                 rectangleBtn.Opacity = 0.6;
             }
+            else
+                tool = null;
         }
 
         private void CircleBtnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -64,6 +111,8 @@ namespace WPF_DCupNote
                 tool = "Circle";
                 circleBtn.Opacity = 0.6;
             }
+            else
+                tool = null;
         }
     }
 }
